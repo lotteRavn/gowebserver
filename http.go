f615e1c8b2e4f4cec2af2d269cf07162c5066918
Finn Nørgaard <finn.norgaard@gmail.com>
2016-08-26 13:05:15 +0000

    Squashed commit of the following:
    commit 67687b204cd2b771190f5cb042051bfc74eb3604
    Author: Finn Nørgaard <finn.norgaard@gmail.com>
    Date:   Fri Aug 26 14:46:59 2016 +0200
    
        Version 02 til alle tal
    
    commit 41645404a81e031186fa2a7c400d35e65e64cc78
    Author: Finn Nørgaard <finn.norgaard@gmail.com>
    Date:   Fri Aug 26 14:41:54 2016 +0200
    
        Version 01.1, tallene 1-4

--- http.go
+++ http.go
@@ -17,7 +17,56 @@ func to_roman(input int)  string {
 
 	if input <1 || input > 3999 {
 		return "Invalid Roman Number Value"}
+
 	s = ""
+	for input >= 1000 {
+		s += "M"
+		input -= 1000
+	}
+	for input >= 900 {
+		s += "CM"
+		input -= 900
+	}
+	for input >= 500 {
+		s += "D"
+		input -= 500
+	}
+	for input >= 400 {
+		s += "CD"
+		input -= 400
+	}
+	for input >= 100 {
+		s += "C"
+		input -= 100
+	}
+	for input >= 90 {
+		s += "XC"
+		input -= 90
+	}
+	for input >= 50 {
+		s += "L"
+		input -= 50
+	}
+	for input >= 40 {
+		s += "XL"
+		input -= 40
+	}
+	for input >= 10 {
+		s += "X"
+		input -= 10
+	}
+	for input >= 9 {
+		s += "IX"
+		input -= 9
+	}
+	for input >= 5 {
+		s += "V"
+		input -= 5
+	}
+	for input >= 4 {
+		s += "IV"
+		input -= 4
+	}
 	for input >= 1 {
 		s += "I"
 		input -= 1
